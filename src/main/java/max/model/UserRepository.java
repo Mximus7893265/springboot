208ffc295e7d06579ac42468b3f39a62ffab5aaa
Max <maximus235711@rambler.ru>
2024-09-29 23:26:38 +0300

    java springboot application

---
+++ src/main/java/max/model/UserRepository.java
@@ -0,0 +1,6 @@
+package max.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<Users, Long> {
+}
---
+++ src/main/java/max/model/Users.java
@@ -0,0 +1,62 @@
+package max.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    public Users() {}
+
+    public Users(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public @NotBlank(message = "Name is mandatory") String getName() {
+        return name;
+    }
+
+    public void setName(@NotBlank(message = "Name is mandatory") String name) {
+        this.name = name;
+    }
+
+    public @NotBlank(message = "Email is mandatory") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank(message = "Email is mandatory") String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
